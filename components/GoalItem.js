ab9a9e1af8492debadfb057e55fdba4efbc65777
YuriyRysak <yuriy.risak@gmail.com>
2023-04-10 13:44:40 +0300

    android_ripple+ios style opacity

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -4,11 +4,15 @@ function GoalItem(props) {
 
 
     return  (
-    <Pressable onPress={props.onDeleteItem.bind(this, props.id)}>
-        <View style={styles.goalItem}>
-          <Text style={styles.goalText}>{props.text}</Text>
-        </View>
-    </Pressable>
+     <View style={styles.goalItem}>
+        <Pressable 
+            android_ripple={{ color: '#210644'}}
+            onPress={props.onDeleteItem.bind(this, props.id)}
+            style={({pressed}) => pressed && styles.pressedItem}
+            >            
+            <Text style={styles.goalText}>{props.text}</Text>            
+        </Pressable>
+    </View>
   );
 }
 
@@ -19,15 +23,20 @@ export default GoalItem;
 
 const styles = StyleSheet.create({
     goalItem: {
-        margin: 8,
-        padding: 8,
+        margin: 8,       
         borderRadius: 6,
         backgroundColor: '#5e0acc',
        
       },
+    pressedItem: {
+        opacity: 0.5
+
+    },  
     
-      goalText: {
-        color: 'white',
-      }
+    goalText: {
+    color: 'white',
+    padding: 8
+    
+    }
 
 });
