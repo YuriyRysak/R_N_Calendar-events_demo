d94a4509f857ee583b84b9b3663571eefeaa83bb
YuriyRysak <yuriy.risak@gmail.com>
2023-04-10 16:12:02 +0300

    add StatusBar end styles button

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -28,6 +28,7 @@ function GoalInput (props) {
     return (
       <Modal visible={props.visible} animationType="slide">
         <View style={styles.inputContainer}>
+         <Image style={styles.image} source={require('../assets/images/goal.png')}/>
           <TextInput 
             style={styles.textInput}
             placeholder='You course goal!'
@@ -36,10 +37,10 @@ function GoalInput (props) {
             />
            <View style={styles.buttonContainer}>
               <View style={styles.button}>
-                <Button title='Add Goal' onPress={addGoalHandler}/>
+                <Button title='Add Goal' onPress={addGoalHandler} color="#b180f0"/>
               </View>
               <View style={styles.button}>
-                <Button title='Cancel' onPress={props.onCancel}/>
+                <Button title='Cancel' onPress={props.onCancel} color="#f31282"/>
               </View>
            </View> 
           
@@ -55,21 +56,33 @@ export default GoalInput;
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        marginBottom: 24,
+        // marginBottom: 24,
         padding: 16,
-        borderBottomColor: "#cccccc",
-        borderBottomWidth: 1
-    
+        // borderBottomColor: "#cccccc",
+        // borderBottomWidth: 1,
+        backgroundColor: '#311b6b'    
       },
+
+      image: {
+        width: 100,
+        height: 100,
+        margin: 20,
+        
+
+      },
+
       textInput: {
         borderWidth: 1,
-        borderColor: '#cccccc',
+        borderColor: '#e4D0ff',
+        backgroundColor:'#e4D0ff',
+        color: '#120438',
+        borderRadius: 6,
         width: '100%',
         // marginRight: 8,
         padding: 8
       },
       buttonContainer: {
-        marginTop: 8,
+        marginTop: 16,
         flexDirection: 'row'
       },
       button: {
