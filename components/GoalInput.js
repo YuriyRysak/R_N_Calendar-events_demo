1ac7acb3b825e2116876a2e8b9550558e48014d7
YuriyRysak <yuriy.risak@gmail.com>
2023-05-08 15:22:28 +0300

    add react-devtools

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -1,93 +1,89 @@
 import { useState } from "react";
-import { 
+import {
   StyleSheet,
-  View, 
-  TextInput, 
+  View,
+  TextInput,
   Button,
   Modal,
   Image,
 } from "react-native";
 
+function GoalInput(props) {
+  const [enteredGoalText, setEnteredGoalText] = useState("");
 
+  function goalInputHandler(enteredText) {
+    setEnteredGoalText(enteredText);
+  }
 
-function GoalInput (props) {
+  function addGoalHandler() {
+    props.onAddGoal(enteredGoalText);
+    setEnteredGoalText("");
+  }
 
-    const [enteredGoalText, setEnteredGoalText] = useState('');
-    
-    function goalInputHandler(enteredText) {
-        setEnteredGoalText(enteredText);
-        
-    }
+  console.log("enteredGoalText");
 
-    function addGoalHandler() {
-        props.onAddGoal(enteredGoalText);
-        setEnteredGoalText('');
-
-    }
-
-    return (
-      <Modal visible={props.visible} animationType="slide">
-        <View style={styles.inputContainer}>
-         <Image style={styles.image} source={require('../assets/images/goal.png')}/>
-          <TextInput 
-            style={styles.textInput}
-            placeholder='You course goal!'
-            onChangeText={goalInputHandler}
-            value={enteredGoalText}   
-            />
-           <View style={styles.buttonContainer}>
-              <View style={styles.button}>
-                <Button title='Add Goal' onPress={addGoalHandler} color="#b180f0"/>
-              </View>
-              <View style={styles.button}>
-                <Button title='Cancel' onPress={props.onCancel} color="#f31282"/>
-              </View>
-           </View> 
-          
+  return (
+    <Modal visible={props.visible} animationType="slide">
+      <View style={styles.inputContainer}>
+        <Image
+          style={styles.image}
+          source={require("../assets/images/goal.png")}
+          // source="/some/path/to/image.png"
+        />
+        <TextInput
+          style={styles.textInput}
+          placeholder="You course goal!"
+          onChangeText={goalInputHandler}
+          value={enteredGoalText}
+        />
+        <View style={styles.buttonContainer}>
+          <View style={styles.button}>
+            <Button title="Add Goal" onPress={addGoalHandler} color="#b180f0" />
+          </View>
+          <View style={styles.button}>
+            <Button title="Cancel" onPress={props.onCancel} color="#f31282" />
+          </View>
         </View>
-
-     </Modal>
-    );
+      </View>
+    </Modal>
+  );
 }
 
 export default GoalInput;
-  const styles = StyleSheet.create({
-    inputContainer: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        // marginBottom: 24,
-        padding: 16,
-        // borderBottomColor: "#cccccc",
-        // borderBottomWidth: 1,
-        backgroundColor: '#311b6b'    
-      },
+const styles = StyleSheet.create({
+  inputContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    // marginBottom: 24,
+    padding: 16,
+    // borderBottomColor: "#cccccc",
+    // borderBottomWidth: 1,
+    backgroundColor: "#311b6b",
+  },
 
-      image: {
-        width: 100,
-        height: 100,
-        margin: 20,
-        
+  image: {
+    width: 100,
+    height: 100,
+    margin: 20,
+  },
 
-      },
-
-      textInput: {
-        borderWidth: 1,
-        borderColor: '#e4D0ff',
-        backgroundColor:'#e4D0ff',
-        color: '#120438',
-        borderRadius: 6,
-        width: '100%',
-        // marginRight: 8,
-        padding: 8
-      },
-      buttonContainer: {
-        marginTop: 16,
-        flexDirection: 'row'
-      },
-      button: {
-        width: 100,
-        marginHorizontal: 8
-
-      }
-  }); 
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#e4D0ff",
+    backgroundColor: "#e4D0ff",
+    color: "#120438",
+    borderRadius: 6,
+    width: "100%",
+    // marginRight: 8,
+    padding: 8,
+  },
+  buttonContainer: {
+    marginTop: 16,
+    flexDirection: "row",
+  },
+  button: {
+    width: 100,
+    marginHorizontal: 8,
+  },
+});
